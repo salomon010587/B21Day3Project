730e81e72445425578a86a2a958b4f41b279fd70
Dzmitryi Zimnitski <scion010587@yahoo.com>
2022-08-01 13:43:54 -0500

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// write your code here...
+        // login logout
         System.out.println("Hello Git Last Day");
     }
 }
